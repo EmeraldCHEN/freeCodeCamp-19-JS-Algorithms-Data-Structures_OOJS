b5f639f8515c1f3caf23908096d5f5a776f1b398
EmeraldNZ <ahyuancy@gmail.com>
2018-12-17 10:26:54 +1300

    Create .js

---
+++ .js
@@ -0,0 +1,34 @@
+							
+function Bird(name)  {
+  this.name= name
+};
+
+let motionModule = (function () {
+  return {
+    glideMixin: function (obj) {
+      obj.glide = function() {
+        console.log(obj.name + " is gliding on the water.");
+      };
+    },
+    flyMixin: function(obj) {
+      obj.fly = function() {
+        console.log("The plane - " + obj.name +" is flying, wooosh!");
+      };
+    }
+  }
+}) (); // The two parentheses cause the function to be immediately invoked
+
+let plane = {
+  name: "A737"
+};
+motionModule.flyMixin(plane);
+
+plane.fly(); // The plane - A737 is flying, wooosh!
+
+let duck = new Bird("The duck");
+motionModule.glideMixin(duck);
+
+duck.glide();  // The duck is gliding on the water.
+
+
+
